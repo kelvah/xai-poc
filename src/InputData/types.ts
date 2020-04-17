e678eda84b247b1738ef9c16626acd24838e77ac
roberto emanuel <kelvah@gmail.com>
2020-04-17 17:24:05 +0200

    Added outcomes mocks

--- src/InputData/types.ts
+++ src/InputData/types.ts
@@ -1,15 +1,15 @@
 export interface IItemObject {
     inputName: string,
     typeRef: string,
-    value?: string | number,
-    components?: (IItemObject | IItemObject[])[],
+    value: string | number | null,
+    components: (IItemObject | IItemObject[])[],
     impact?: boolean | number,
     score?: number
 }
 
 export interface IInputRow {
     inputLabel: string,
-    inputValue?: string | number,
+    inputValue?: string | number | null,
     hasEffect?: boolean | number,
     score?: number,
     key: string,
